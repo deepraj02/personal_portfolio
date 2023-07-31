44cedd4bd903ac6ce42bdbb8924e49b4f4e5dea1
Deepraj Baidya <deeprajbaidya@gmail.com>
2023-07-31 22:30:24 +0530

    🛠  REFACTOR: proj architecture config  -v1

---
+++ lib/src/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Portfolio extends StatelessWidget {
+  const Portfolio({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
