df7942a7c2fab87217ba1ab74d3042d73e82b5f8
Deepraj Baidya <deeprajbaidya@gmail.com>
2023-08-02 18:34:10 +0530

    📦 FEAT: shell navigation config -v1

--- lib/src/features/splash/presentation/pages/thesplash.page.dart
+++ lib/src/features/splash/presentation/pages/thesplash.page.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:personal_protfolio/src/config/keys.config.dart';
 import 'package:personal_protfolio/src/features/splash/presentation/widgets/loading.widget.dart';
+import 'package:personal_protfolio/src/features/welcome/presentation/pages/welcome.page.dart';
 
 import '../../../../config/colors_config.dart';
-import '../../../shell/presentation/pages/portfoliomain.page.dart';
 
 class Splash extends StatelessWidget {
   static const String route = '/splash';
@@ -15,7 +15,7 @@ class Splash extends StatelessWidget {
   Widget build(BuildContext context) {
     Future.delayed(const Duration(seconds: 2), () {
       GoRouter.of(GlobalKeysConfig.mainNav.currentContext!).go(
-        PortfolioMainPage.route,
+        WelcomePage.route,
       );
     });
     return const Scaffold(
--- lib/src/routes/route_config.dart
+++ lib/src/routes/route_config.dart
@@ -3,24 +3,37 @@ import 'package:personal_protfolio/src/config/keys.config.dart';
 import 'package:personal_protfolio/src/features/shell/presentation/pages/portfoliomain.page.dart';
 
 import '../features/splash/presentation/pages/thesplash.page.dart';
+import '../features/welcome/presentation/pages/welcome.page.dart';
 
 class AppRoutes {
   static final router = GoRouter(
+    ///@ [Initial Entry] Point for the App
 
-      ///@ [Initial Entry] Point for the App
-
-      initialLocation: Splash.route,
-      navigatorKey: GlobalKeysConfig.mainNav,
-      routes: [
-        GoRoute(
-          parentNavigatorKey: GlobalKeysConfig.mainNav,
-          path: Splash.route,
-          builder: (context, state) => const Splash(),
-        ),
-        GoRoute(
-          parentNavigatorKey: GlobalKeysConfig.mainNav,
-          path: PortfolioMainPage.route,
-          builder: (context, state) => const PortfolioMainPage(),
-        ),
-      ]);
+    initialLocation: Splash.route,
+    navigatorKey: GlobalKeysConfig.mainNav,
+    routes: [
+      GoRoute(
+        parentNavigatorKey: GlobalKeysConfig.mainNav,
+        path: Splash.route,
+        builder: (context, state) => const Splash(),
+      ),
+      ShellRoute(
+        navigatorKey: GlobalKeysConfig.tabNav,
+        builder: ((context, state, child) {
+          return PortfolioMainPage(child: child);
+        }),
+        routes: [
+          GoRoute(
+            parentNavigatorKey: GlobalKeysConfig.tabNav,
+            path: WelcomePage.route,
+            pageBuilder: (context, state) {
+              return const NoTransitionPage(
+                child: WelcomePage(),
+              );
+            },
+          ),
+        ],
+      ),
+    ],
+  );
 }
