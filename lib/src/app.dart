b981150cbc47a7f9c9ca66ea6d0d03dcc534b3a0
Deepraj Baidya <deeprajbaidya@gmail.com>
2023-08-02 14:16:35 +0530

    🛠  REFACTOR: initial route nav to Splash

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:personal_protfolio/src/features/splash/presentation/pages/thesplash.page.dart';
 
 class Portfolio extends StatelessWidget {
   const Portfolio({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Splash();
   }
 }
