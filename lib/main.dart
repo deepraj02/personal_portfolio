ec34f304fe5141a0bcea9434fa699685ea59e028
Deepraj Baidya <deeprajbaidya@gmail.com>
2023-08-02 19:17:49 +0530

    🛠  REFACTOR: changes for riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:personal_protfolio/src/routes/route_config.dart';
 
 void main() {
-  runApp(const Damn());
+  runApp(
+    const ProviderScope(
+      child: Damn(),
+    ),
+  );
 }
 
 class Damn extends StatelessWidget {
