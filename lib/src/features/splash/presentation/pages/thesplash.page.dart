168a4a0731e210f737fed4ed42fab0f085a1dee9
Deepraj Baidya <deeprajbaidya@gmail.com>
2023-08-02 17:21:31 +0530

    📦 FEAT: animation delay & route init #3

--- lib/src/features/splash/presentation/pages/thesplash.page.dart
+++ lib/src/features/splash/presentation/pages/thesplash.page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:personal_protfolio/src/config/keys.config.dart';
 import 'package:personal_protfolio/src/features/splash/presentation/widgets/loading.widget.dart';
 
 import '../../../../config/colors_config.dart';
+import '../../../shell/presentation/pages/portfoliomain.page.dart';
 
 class Splash extends StatelessWidget {
   static const String route = '/splash';
@@ -10,6 +13,11 @@ class Splash extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Future.delayed(const Duration(seconds: 2), () {
+      GoRouter.of(GlobalKeysConfig.mainNav.currentContext!).go(
+        PortfolioMainPage.route,
+      );
+    });
     return const Scaffold(
       backgroundColor: TheColors.mainBlue,
       body: SplashLoading(
