bf69048f1bfec45efc35ef4c81d1325443151abf
Deepraj Baidya <deeprajbaidya@gmail.com>
2023-08-08 20:02:27 +0530

    📦 FEAT: githubPage UI --v6 (Complete)

--- lib/src/features/github/presentation/pages/github.page.dart
+++ lib/src/features/github/presentation/pages/github.page.dart
@@ -1,13 +1,98 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:personal_protfolio/src/features/github/presentation/providers/githubpage.riverpod.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class GithubPage extends StatelessWidget {
+import '../../../../config/colors_config.dart';
+import '../../../../shared/widgets/error.page.dart';
+
+class GithubPage extends ConsumerWidget {
   static const String route = '/github';
   const GithubPage({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Github Page'),
+@override
+  Widget build(BuildContext context, WidgetRef ref) {
+    var githubDataAsync = ref.watch(githubProvider);
+    return githubDataAsync.when(
+      loading: () => const Center(
+          child: CircularProgressIndicator(
+        valueColor: AlwaysStoppedAnimation(Colors.white),
+      )),
+      error: (error, stackTrace) => const Text('error'),
+      data: (githubData) {
+        return Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                githubData.icon,
+                size: 100,
+                color: TheColors.githubIcon,
+              ),
+              Text.rich(
+                TextSpan(
+                    style: const TextStyle(fontSize: 100, color: Colors.white),
+                    children: [
+                      //const TextSpan(text: "I'm "),
+                      TextSpan(
+                        text: githubData.heading,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ]),
+                textAlign: TextAlign.center,
+              ),
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                     
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(30),
+                        child: Container(
+                          color: TheColors.githubSecondary,
+                          child: GestureDetector(
+                            onTap: () async {
+                              var myGithubUrl = Uri.parse(githubData.url);
+                              if (!await launchUrl(myGithubUrl)) {
+                                const ErrorPage(
+                                  errorMessage: "Could not Launch URL",
+                                );
+                              }
+                            },
+                            child: Padding(
+                              padding: const EdgeInsets.all(20.0),
+                              child: Text(
+                                githubData.subHeading,
+                                textAlign: TextAlign.center,
+                                style: const TextStyle(
+                                  fontSize: 30,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
