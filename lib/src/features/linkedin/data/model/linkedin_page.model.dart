2c617f8fad422cd219b65748d554720ddcb36cac
Deepraj Baidya <deeprajbaidya@gmail.com>
2023-08-08 16:21:54 +0530

    📦 FEAT: LinkedinPage Data models

---
+++ lib/src/features/linkedin/data/model/linkedin_page.model.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+class LinkedinPageModel {
+  final IconData icon;
+  final String title;
+  final String handle;
+  final String url;
+  LinkedinPageModel({
+    required this.icon,
+    required this.title,
+    required this.handle,
+    required this.url,
+  });
+}
