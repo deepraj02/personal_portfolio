69dcf2b15ce6610e06bfa25dd7ff8a4adcfaad48
Deepraj Baidya <deeprajbaidya@gmail.com>
2023-08-08 19:51:55 +0530

    📦 FEAT: githubPage ViewModels -v5

---
+++ lib/src/features/github/presentation/viewmodels/githubpage.viewmodels.dart
@@ -0,0 +1,20 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:personal_protfolio/src/features/github/data/model/githubpage.models.dart';
+
+import 'package:personal_protfolio/src/features/github/data/repository/igithubpage.repository.dart';
+
+class GithubPageViewModels {
+  final Ref ref;
+  final IGithubPageRepository githubRepository;
+
+  GithubPageViewModels({
+    required this.ref,
+    required this.githubRepository,
+  });
+
+  Future<GitHubPageModel> getGithubPageData() async {
+    var githubPageData = await githubRepository.getGithubPageData();
+    return githubPageData;
+  }
+}
