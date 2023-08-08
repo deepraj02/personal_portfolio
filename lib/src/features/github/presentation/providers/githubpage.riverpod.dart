03473ee4bde8b6a9e5eae40b8840912e919417f2
Deepraj Baidya <deeprajbaidya@gmail.com>
2023-08-08 19:55:17 +0530

    📦 FEAT: githubPage providers -v6

--- lib/src/features/github/presentation/providers/githubpage.riverpod.dart
+++ lib/src/features/github/presentation/providers/githubpage.riverpod.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:personal_protfolio/src/features/github/presentation/viewmodels/githubpage.viewmodels.dart';
 import '../../data/repository/mockgithubpage.repository.dart';
 import '../../data/repository/igithubpage.repository.dart';
 
@@ -6,3 +7,12 @@ final githubRepositoryProvider = Provider<IGithubPageRepository>((ref) {
   return MockGithubRepository();
 });
 
+final twitterViewModelProvider = Provider<GithubPageViewModels>((ref) {
+  var repository = ref.read(githubRepositoryProvider);
+  return GithubPageViewModels(ref: ref, githubRepository: repository);
+});
+
+final githubVMProvider = FutureProvider((ref) {
+  var githubVM = ref.read(twitterViewModelProvider);
+  return githubVM.getGithubPageData();
+});
