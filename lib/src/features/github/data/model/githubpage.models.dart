b1d2e68e5fbe4a8050ca17e0bbc3c0e3f7bc338d
Deepraj Baidya <deeprajbaidya@gmail.com>
2023-08-08 19:38:13 +0530

    📦 FEAT: githubPage -models (data layr.) -v1

---
+++ lib/src/features/github/data/model/githubpage.models.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+class GitHubPageModel {
+  final IconData icon;
+  final String heading;
+  final String subHeading;
+  final String url;
+  GitHubPageModel({
+    required this.icon,
+    required this.heading,
+    required this.subHeading,
+    required this.url,
+  });
+}
