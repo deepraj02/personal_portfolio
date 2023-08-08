8738737173e6c18ffab1820c9bbd05645e71a0e0
Deepraj Baidya <deeprajbaidya@gmail.com>
2023-08-08 16:28:06 +0530

    🎨 STYLE: LinkedinPage done -v1

--- lib/src/features/linkedin/presentation/pages/linkedin.page.dart
+++ lib/src/features/linkedin/presentation/pages/linkedin.page.dart
@@ -1,13 +1,88 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:personal_protfolio/src/features/linkedin/presentation/providers/linkedin_page.riverpod.dart';
 
-class LinkedinPage extends StatelessWidget {
+import '../../../../config/colors_config.dart';
+
+class LinkedinPage extends ConsumerWidget {
   static const String route = '/linkedin';
   const LinkedinPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Linkedin Page'),
+  Widget build(BuildContext context, WidgetRef ref) {
+    var linkedinDataAsync = ref.watch(linkedinProvider);
+    return linkedinDataAsync.when(
+      loading: () => const Center(
+          child: CircularProgressIndicator(
+        valueColor: AlwaysStoppedAnimation(Colors.white),
+      )),
+      error: (error, stackTrace) => const Text('error'),
+      data: (twitterData) {
+        return Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                twitterData.icon,
+                size: 100,
+                color: TheColors.twitterIcon,
+              ),
+              Text.rich(
+                TextSpan(
+                    style: const TextStyle(fontSize: 100, color: Colors.white),
+                    children: [
+                      //const TextSpan(text: "I'm "),
+                      TextSpan(
+                        text: twitterData.title,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ]),
+                textAlign: TextAlign.center,
+              ),
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(30),
+                        child: Container(
+                          color: TheColors.twitterSecondary,
+                          child: GestureDetector(
+                            child: Padding(
+                              padding: const EdgeInsets.all(20.0),
+                              child: Text(
+                                twitterData.handle,
+                                textAlign: TextAlign.center,
+                                style: const TextStyle(
+                                  fontSize: 30,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
