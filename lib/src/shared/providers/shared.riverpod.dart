f800a702764a24d99d5a27de87c60aacc38b2859
Deepraj Baidya <deeprajbaidya@gmail.com>
2023-08-13 14:25:09 +0530

    📦 FEAT: firebase firestore providers

--- lib/src/shared/providers/shared.riverpod.dart
+++ lib/src/shared/providers/shared.riverpod.dart
@@ -1,9 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:personal_protfolio/src/config/colors_config.dart';
 
 import '../../features/welcome/presentation/pages/welcome.page.dart';
+import '../repositories/database_provider.dart';
+import '../repositories/idb_provider.dart';
 
 final pageColorProvider = StateProvider<LinearGradient>((ref) {
   return TheColors.pageColor[WelcomePage.route]!;
+});
+
+final firebaseDbInstanceProvider = Provider<FirebaseFirestore>((ref) {
+  return FirebaseFirestore.instance;
+});
+
+final dbProvider = Provider<IDBProvider>((ref) {
+  return DatabaseProvider(ref);
 });
