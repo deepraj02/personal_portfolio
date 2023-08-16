69e887816e68aa2e63e09015ecbb30ff68bef966
Deepraj Baidya <deeprajbaidya@gmail.com>
2023-08-16 16:47:46 +0530

    📦 FEAT: provider to fetch firebase data

--- lib/src/features/twitter/presentation/providers/twitter_page.riverpod.dart
+++ lib/src/features/twitter/presentation/providers/twitter_page.riverpod.dart
@@ -1,10 +1,10 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:personal_protfolio/src/features/twitter/presentation/viewmodels/twitter.viewmodels.dart';
 import '../../data/repository/itwitterpage.repository.dart';
-import '../../data/repository/mocktwitterpage.repository.dart';
+import '../../data/repository/twitterpage.repository.dart';
 
 final twitterRepositoryProvider = Provider<ITwitterPageRepository>((ref) {
-  return MockTwitterRepository();
+  return TwitterRepository(ref);
 });
 
 final twitterViewModelProvider = Provider<TwitterPageViewModels>((ref) {
