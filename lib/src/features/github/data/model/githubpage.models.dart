85a9ca5dc018d8eba8353092c8026fcdefc3d43c
Deepraj Baidya <deeprajbaidya@gmail.com>
2023-08-19 21:30:21 +0530

    📦 FEAT: title prop. to fit in the details

--- lib/src/features/github/data/model/githubpage.models.dart
+++ lib/src/features/github/data/model/githubpage.models.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
+
 import '../../../../config/icons.config.dart';
 
 class GitHubPageModel {
@@ -7,15 +8,18 @@ class GitHubPageModel {
   final String heading;
   final String subHeading;
   final String url;
+  final String title;
   GitHubPageModel({
     required this.icon,
     required this.heading,
     required this.subHeading,
     required this.url,
+    required this.title,
   });
   factory GitHubPageModel.fromDatabase(Map<String, dynamic> doc) {
     return GitHubPageModel(
       icon: PersonalPortfolioIcons.iconMap[doc['icon']]!,
+      title:doc['title'],
       url: doc['url'],
       heading: doc['heading'],
       subHeading: doc['subHeading'],
