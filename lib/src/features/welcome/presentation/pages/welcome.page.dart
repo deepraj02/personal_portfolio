b788b8ff120db0891a86ebeaec59c1b327707147
Deepraj Baidya <deeprajbaidya@gmail.com>
2023-08-28 22:22:34 +0530

    📦 FEAT: welcomePage responsiveness fix

--- lib/src/features/welcome/presentation/pages/welcome.page.dart
+++ lib/src/features/welcome/presentation/pages/welcome.page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../../../../../helpers/responsive_ui_helper.dart';
+import '../../../../shared/widgets/error_widget.dart';
+import '../../responsive/welcome_page_responsive.dart';
 import '../widgets/greetings_label.dart';
 
 import '../../../../config/colors_config.dart';
@@ -12,102 +15,90 @@ class WelcomePage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    var uiConfig = WelcomePageResponsiveConfig
+        .responsiveUI[ResponsiveUIHelper.getDeviceType(context)]!;
     var welcomeDataAsync = ref.watch(welcomeProvider);
 
     return welcomeDataAsync.when(
-      loading: () => const Center(
-          child: CircularProgressIndicator(
-        valueColor: AlwaysStoppedAnimation(Colors.white),
-      )),
-      error: (error, stackTrace) => const Text('error'),
-      data: (welcomeData) {
-        return Center(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    width: 300,
-                    height: 300,
-                    decoration: BoxDecoration(
-                      border:
-                          Border.all(color: TheColors.welcomePrimary, width: 8),
-                      shape: BoxShape.circle,
-                      image: DecorationImage(
-                          image: NetworkImage(welcomeData.imgPath),
-                          fit: BoxFit.cover),
-                    ),
-                  ),
-                  const SizedBox(width: 40),
-                ],
-              ),
-              const GreetingsLabel(),
-              Text.rich(
-                TextSpan(
-                    style: const TextStyle(fontSize: 100, color: Colors.white),
-                    children: [
-                      const TextSpan(text: "I'm "),
+        loading: () => const Center(
+                child: CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation(Colors.white),
+            )),
+        error: (error, stackTrace) =>
+            ErrorNotification(message: error.toString()),
+        data: (welcomeData) {
+          return Center(
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Flex(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        direction: uiConfig.headerAxis,
+                        children: [
+                          Container(
+                              width: uiConfig.imageSize,
+                              height: uiConfig.imageSize,
+                              decoration: BoxDecoration(
+                                  border: Border.all(
+                                      color: TheColors.welcomePrimary,
+                                      width: uiConfig.imageBorderSize),
+                                  shape: BoxShape.circle,
+                                  image: DecorationImage(
+                                      image: NetworkImage(welcomeData.imgPath),
+                                      fit: BoxFit.cover))),
+                          uiConfig.headerGap,
+                        ]),
+                    const GreetingsLabel(),
+                    Text.rich(
                       TextSpan(
-                          text: welcomeData.name,
-                          style: const TextStyle(fontWeight: FontWeight.bold)),
-                    ]),
-                textAlign: TextAlign.center,
-              ),
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const SizedBox(width: 20),
-                  Column(
-                    mainAxisSize: MainAxisSize.min,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const SizedBox(
-                        height: 30,
-                      ),
-                      Text(
-                        welcomeData.title,
-                        textAlign: TextAlign.center,
-                        style: const TextStyle(
-                          fontSize: 40,
-                          color: Colors.white,
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 30,
-                      ),
-                      Text(
-                        welcomeData.subTitle,
-                        textAlign: TextAlign.center,
-                        style: const TextStyle(
-                          fontSize: 30,
-                          color: Colors.black,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ]
-                .animate(
-                  interval: 100.ms,
-                )
-                .slideY(
-                  begin: 1,
-                  end: 0,
-                  duration: 0.5.seconds,
-                  curve: Curves.easeInOut,
-                )
-                .fadeIn(),
-          ),
-        );
-      },
-    );
+                          style: TextStyle(
+                              fontSize: uiConfig.titleSize,
+                              color: Colors.white),
+                          children: [
+                            const TextSpan(text: "I'm "),
+                            TextSpan(
+                                text: welcomeData.name,
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.bold)),
+                          ]),
+                      textAlign: TextAlign.center,
+                    ),
+                    Flex(
+                        direction: uiConfig.headerAxis,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          uiConfig.subtitleGap,
+                          Column(
+                            mainAxisSize: MainAxisSize.min,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(welcomeData.title,
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                      fontSize: uiConfig.subTitleSize,
+                                      color: Colors.white)),
+                              uiConfig.headerGap,
+                              Text(welcomeData.subTitle,
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                      fontSize: uiConfig.subTitleSize,
+                                      color:
+                                          const Color.fromARGB(255, 0, 0, 0))),
+                            ],
+                          )
+                        ])
+                  ]
+                      .animate(interval: 100.ms)
+                      .slideY(
+                          begin: 1,
+                          end: 0,
+                          duration: 0.5.seconds,
+                          curve: Curves.easeInOut)
+                      .fadeIn()));
+        });
   }
 }
