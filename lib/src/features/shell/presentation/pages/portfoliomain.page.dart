924770e418399d8d57a694ba35e1884c5a237a65
Deepraj Baidya <deeprajbaidya@gmail.com>
2023-09-23 23:57:56 +0530

    🚀 BUILD: sidebar- menu configured fully

--- lib/src/features/shell/presentation/pages/portfoliomain.page.dart
+++ lib/src/features/shell/presentation/pages/portfoliomain.page.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 
+import '../../../../../helpers/responsive_ui_helper.dart';
 import '../../../../config/colors_config.dart';
 import '../../../../config/keys.config.dart';
 import '../../../../shared/widgets/pagecolor.dart';
+import '../../../navigation/presentation/responsive/navigation_responsive.config.dart';
 import '../../../navigation/presentation/widgets/left_navigation.dart';
+import '../../../navigation/presentation/widgets/side_navigation_drawer.dart';
 
 class PortfolioMainPage extends StatelessWidget {
   static const String route = '/main';
@@ -14,16 +17,26 @@ class PortfolioMainPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         key: GlobalKeysConfig.mainScaffold,
+        drawer: const SideNavigationDrawer(),
         backgroundColor: TheColors.mainBlue,
-        body: Stack(
-          children: [
-            const PageColor(),
-            Center(child: child),
-            const Align(
-              alignment: Alignment.centerLeft,
-              child: LeftNavigation(),
-            ),
-          ],
-        ));
+        body: Builder(builder: (context) {
+          var navUIConfig = NavigationResponsiveConfig
+              .responsiveUI[ResponsiveUIHelper.getDeviceType(context)]!;
+
+          if (!navUIConfig.showSidebar &&
+              GlobalKeysConfig.mainScaffold.currentState!.isDrawerOpen) {
+            GlobalKeysConfig.mainScaffold.currentState!.closeDrawer();
+          }
+          return Stack(
+            children: [
+              const PageColor(),
+              Center(child: child),
+              const Align(
+                alignment: Alignment.centerLeft,
+                child: LeftNavigation(),
+              ),
+            ],
+          );
+        }));
   }
 }
